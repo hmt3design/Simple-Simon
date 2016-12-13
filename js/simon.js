17a9463d87ddb87a628856200f711fca84517f60
hmt3design <hmt3design@gmail.com>
2016-12-13 16:00:04 -0600

    Simon files 121316 - PM

--- js/simon.js
+++ js/simon.js
@@ -1,55 +1,91 @@
 /**
  * Created by Harry on 12/9/16.
  */
-$(document).ready(function(){
+// $(document).ready(function(){
     "use strict";
 
-    var currentArray = [];
-    var level = 0;
+    var simonArray = [];
     var squares = $('.square');
+    var counter = 0;
+    var enableClicks = false;
+    var sequence;
 
     //Create click function for buttons
-    $(".btn").click(function(){
-        $(this).addClass("active");
+    var userTurn = function () {
+      enableClicks = true;
+    };
+
+    // user turn
+    $(".square").click(function(){
+        var squareClicked = this;
+        if (enableClicks) {
+            animate(squareClicked);
+            if(squareClicked === simonArray[counter]) {
+                counter += 1;
+                if (counter === simonArray.length) {
+                    // reset counter
+                    counter = 0;
+                    // success
+                    generateSimonsChoice();
+                    setTimeout(function () {
+                        simonTurn(simonArray);
+                    }, 1000);
+                }
+            } else {
+                alert("Sorry, you entered the wrong sequence. Game over.");
+                function clearGame() {
+                    simonArray = [];
+                    counter = 0;
+                    $("#start").prop("disabled", false);
+
+                }
+
+                // gameover
+            }
+        }
     });
 
     // Generate animation for Simon's turn
     var animate = function (square) {
-        $(square).addClass("btn:active");
+        $(square).addClass("active");
         setTimeout(function () {
-            $(square).removeClass("btn:active")
-        }, 1000);
+            $(square).removeClass("active")
+        }, 250);
     };
 
+    // var playSequence = function() {
+    //     var i = 0;
+    //     sequence = setInterval(function () {
+    //         animate(simonArray[i]);
+    //         console.log("test");
+    //         i++;
+    //         if (i >= simonArray.length) {
+    //             clearInterval(sequence);
+    //             i = 0;
+    //         }
+    //     }, 2000);
+    // };
+
+    //adds Simon's squares to array
     var generateSimonsChoice = function () {
-        currentArray.push(squares[Math.floor(Math.random()*4)]);
+        simonArray.push(squares[Math.floor(Math.random()*4)]);
     };
 
-    $("#easy").click(function () {
-        generateSimonsChoice(10);
-        simonTurn(currentArray);
-        // checkArray();
-        console.log(currentArray);
+    $("#start").click(function () {
+        generateSimonsChoice();
+        simonTurn(simonArray);
+        $("#start").prop("disabled", true);
     });
-
-    //Generate intermediate array function that generates when Intermediate button is clicked
-    $("#intermediate").click(function () {
-        currentArray = Array.from({length: 20}, () => Math.floor(Math.random()*4)+1);
-        // checkArray();
-    });
-
-    //Generate hard array function that generates when Hard button is clicked
-    $("#hard").click(function () {
-        currentArray = Array.from({length: 30}, () => Math.floor(Math.random()*4)+1);
-        // checkArray();
-    });
-
 
     var simonTurn = function (simonArray) {
-        simonArray.forEach(function(square) {
-            animate(square);
+        var timer = 500;
+        simonArray.forEach(function(square, i) {
+            setTimeout(function () {
+                animate(square);
+            }, timer * i);
+            console.log(square);
         });
+        userTurn();
     };
-    // });
 
-});
+// });
