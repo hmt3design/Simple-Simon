9add0fe202937995ca7a3948b8309b9c9385830b
hmt3design <hmt3design@gmail.com>
2016-12-14 10:55:03 -0600

    Simon files 121416 - Demo version, 1.5

--- js/simon.js
+++ js/simon.js
@@ -1,14 +1,13 @@
 /**
  * Created by Harry on 12/9/16.
  */
-// $(document).ready(function(){
+$(document).ready(function(){
     "use strict";
-
+    //Global variables
     var simonArray = [];
     var squares = $('.square');
     var counter = 0;
     var enableClicks = false;
-    var sequence;
 
     //Create click function for buttons
     var userTurn = function () {
@@ -22,6 +21,7 @@
             animate(squareClicked);
             if(squareClicked === simonArray[counter]) {
                 counter += 1;
+                document.getElementById('counter').innerHTML = counter;
                 if (counter === simonArray.length) {
                     // reset counter
                     counter = 0;
@@ -31,16 +31,12 @@
                         simonTurn(simonArray);
                     }, 1000);
                 }
-            } else {
-                alert("Sorry, you entered the wrong sequence. Game over.");
-                function clearGame() {
+            } else {  // game over - reset all values and restart
+                alert("Sorry, you entered the wrong sequence. Game over. Want to try again?");
                     simonArray = [];
                     counter = 0;
+                    document.getElementById("counter").innerHTML = counter;
                     $("#start").prop("disabled", false);
-
-                }
-
-                // gameover
             }
         }
     });
@@ -53,30 +49,20 @@
         }, 250);
     };
 
-    // var playSequence = function() {
-    //     var i = 0;
-    //     sequence = setInterval(function () {
-    //         animate(simonArray[i]);
-    //         console.log("test");
-    //         i++;
-    //         if (i >= simonArray.length) {
-    //             clearInterval(sequence);
-    //             i = 0;
-    //         }
-    //     }, 2000);
-    // };
-
     //adds Simon's squares to array
     var generateSimonsChoice = function () {
         simonArray.push(squares[Math.floor(Math.random()*4)]);
     };
 
+    //Start button
     $("#start").click(function () {
         generateSimonsChoice();
         simonTurn(simonArray);
         $("#start").prop("disabled", true);
     });
 
+    //Simon's turn, which feeds into the user's turn
+    //"square" passes the div id to the animate function, so the colors don't have to be specifically identified
     var simonTurn = function (simonArray) {
         var timer = 500;
         simonArray.forEach(function(square, i) {
@@ -88,4 +74,4 @@
         userTurn();
     };
 
-// });
+});
